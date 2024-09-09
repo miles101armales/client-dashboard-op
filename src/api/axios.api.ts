362a0458ba8198fe06e97cff10f98223eaa056db
armales <armansk8er@gmail.com>
2024-09-10 03:14:39 +0500

    gh-pages8

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-	baseURL: 'http://45.131.96.9:3000',
+	baseURL: 'http://45.131.96.9:3000/dashboard',
 	headers: {
 		Accept: '*/*'
 	}
